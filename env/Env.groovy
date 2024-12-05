2624a7f7bec4fb6317585c81a323963800bd94fb
Gannon Bardasian <fbardasian@gmail.com>
2024-12-04 16:41:05 -0800

    Enviorment Completed... (maybe/hopefully)

--- env/Env.groovy
+++ env/Env.groovy
@@ -1,5 +1,6 @@
 package env
 import values.Value
+
 class Env {
     Map<String, Value> env = new HashMap<>();
 
@@ -8,6 +9,17 @@ class Env {
             throw new Exception("AAQZ this id does not have a value")
         }
         return env.get(id)
+    }
+    
+    void extendEnv(List<String> ids, List<Value> vals) {
+        if (ids.size() == vals.size()) {
+            for (int i = 0; i < ids.size(); i++) {
+                env.put(ids.get(i) as String, vals.get(i) as Value)
+            }
+        }
+        else {
+            throw new Exception("AAQZ Lists sizes did not match")
+        }
     }
 
     
